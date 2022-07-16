ad5aaed38138d2c27b8c3b98e07e6bde63597257
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-07-16 17:40:26 +0300

    16-07-2022

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const main = document.querySelector(".main");
 const starcraft = document.querySelector(".starcraft");
 
+// Çarpma esnasında ses çıkarmak içim koymuştum fakat henüz tamamlamadım
 // let king = new Audio("./sounds")
 
 let move;
 let x, y;
-let velocity;
 
+//arka planda olan yıldızları oluşturmak için kullandığım for döngüsü
 for (let a = 1; a <= 1000; a++) {
   const star = document.createElement("div");
   let x = Math.floor(Math.random() * window.innerWidth);
@@ -19,25 +20,30 @@ for (let a = 1; a <= 1000; a++) {
   let starsize = Math.floor(Math.random() * 3);
   star.style.height = starsize + "px";
   star.style.width = starsize + "px";
+
+  //yıldızların yoğunluğunu belirtmek için kullandım
   star.style.background = starsize < 4 ? "lightgray" : "white";
   main.appendChild(star);
 }
 
+// roketin yön tuşları ile sağa ve sola hareket etmesi için oluşturuldu.
 window.addEventListener("keydown", (e)=>{
 
     var left = parseInt(window.getComputedStyle(starcraft).getPropertyValue("left"));
-
+    let ScreenWidth=window.screen.width;
+    console.log(ScreenWidth);
     if(e.key =="ArrowLeft" && left >0 ){
         starcraft.style.left = left - 10 + "px";
     }
 
-    
-    else if(e.key =="ArrowRight" && left <=460 ){
+    //burayı ScreenWidth-80 olarak değiştirmemdeki sebep roketin sadece yarıya kadar degil de tüm sayfa boyunca gidip gelmesini sağlamaktı
+    else if(e.key =="ArrowRight" && left <=(ScreenWidth-80) ){
         starcraft.style.left = left + 10 + "px";
     }
 
 });
 
+// karakterimiz olan ragnarın rastgele konumlarda üretilip ekranda gelmesini saglayan fonksiyondur.
 const ragnarmove = () =>{
     move = setInterval(()=>{
         let ragnar = document.createElement("div");
